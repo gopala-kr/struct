f6fce90598bb095b157dbdc9cf9db05f1b97d2b8
Gopala KR <gopal.rms4@gmail.com>
2017-06-17 17:20:19 +0530

    Add files via upload

---
+++ duckduckgo-master/lib/DDG/Test/Goodie.pm
@@ -0,0 +1,99 @@
+package DDG::Test::Goodie;
+# ABSTRACT: Adds keywords to easily test Goodie plugins.
+
+use strict;
+use warnings;
+use Carp;
+use Test::More;
+use Test::Deep;
+use DDG::Test::Block;
+use DDG::ZeroClickInfo;
+use Package::Stash;
+
+binmode STDOUT, ':utf8';
+binmode Test::More->builder->output, ':utf8';
+binmode Test::More->builder->failure_output, ':utf8';
+
+=head1 DESCRIPTION
+
+Installs functions for testing Goodies.
+
+B<Warning>: Be aware that you only use this module inside your test files in B<t/>.
+
+=cut
+
+sub import {
+	my ( $class, %params ) = @_;
+	my $target = caller;
+	my $stash = Package::Stash->new($target);
+
+=keyword test_zci
+
+Easy function to generate a L<DDG::ZeroClickInfo> for the test. See
+L</ddg_goodie_test>.
+
+You can predefine parameters via L</zci>.
+
+=cut
+
+	my %zci_params;
+
+	$stash->add_symbol('&test_zci', sub {
+		my $answer = shift;
+		ref $_[0] eq 'HASH' ?
+			DDG::ZeroClickInfo->new(%zci_params, %{$_[0]}, answer => $answer ) :
+			DDG::ZeroClickInfo->new(%zci_params, @_, answer => $answer )
+	});
+
+=keyword zci
+
+You can predefine L<DDG::ZeroClickInfo> parameters for usage in L</test_zci>.
+
+This function can be used several times to change specific defaults on the
+fly.
+
+=cut
+
+	$stash->add_symbol('&zci', sub {
+		if (ref $_[0] eq 'HASH') {
+			for (keys %{$_[0]}) {
+				$zci_params{$_} = $_[0]->{$_};
+			}
+		} else {
+			while (@_) {
+				my $key = shift;
+				my $value = shift;
+				$zci_params{$key} = $value;
+			}
+		}
+	});
+
+=keyword ddg_goodie_test
+
+With this function you can easily generate a small own L<DDG::Block> for
+testing your L<DDG::Goodie> alone or in combination with others.
+
+  ddg_goodie_test(
+	[qw( DDG::Goodie::MyGoodie )],
+	'mygooodie data' => test_zci('data', html => '<div>data</div>'),
+	'mygooodie data2' => test_zci('data2', html => '<div>data2</div>'),
+  );
+
+=cut
+
+	$stash->add_symbol('&ddg_goodie_test', sub { block_test(sub {
+			my ($query, $answer, $zci) = @_;
+		subtest "Query: $query" => sub {
+			if ($answer) {
+				$zci->{caller} = $answer->caller;    # TODO: Review all this cheating; seriously.
+				cmp_deeply($answer,$zci,'Deep: full ZCI object');
+			} else {
+				fail('Expected result but dont get one on '.$query) unless defined $answer;
+			}
+		};
+		},@_)
+	});
+
+}
+
+1;
---
+++ duckduckgo-master/lib/DDG/Test/Spice.pm
@@ -0,0 +1,132 @@
+package DDG::Test::Spice;
+# ABSTRACT: Adds keywords to easily test Spice plugins.
+
+use strict;
+use warnings;
+use Carp;
+use Test::More;
+use DDG::Test::Block;
+use DDG::ZeroClickInfo::Spice;
+use DDG::Meta::ZeroClickInfoSpice;
+use Package::Stash;
+use Class::Load 'load_class';
+
+=head1 DESCRIPTION
+
+Installs functions for testing Spice.
+
+B<Warning>: Be aware that you only use this module inside your test files in B<t/>.
+
+=cut
+
+sub import {
+	my ( $class, %params ) = @_;
+	my $target = caller;
+	my $stash = Package::Stash->new($target);
+
+=keyword test_spice
+
+Easy function to generate a L<DDG::ZeroClickInfo::Spice> for the test. See
+L</ddg_spice_test>.
+
+You can predefine parameters via L</spice>.
+
+The first parameter gets treated as the
+L<call of the DDG::ZeroClickInfo::Spice|DDG::ZeroClickInfo::Spice/call>
+
+=cut
+
+	my %spice_params = (
+		call_type => 'include',
+	);
+
+	$stash->add_symbol('&test_spice', sub {
+		my $call = shift;
+		ref $_[0] eq 'HASH'
+			? DDG::ZeroClickInfo::Spice->new(%spice_params, %{$_[0]}, call => $call )
+			: DDG::ZeroClickInfo::Spice->new(%spice_params, @_, call => $call )
+	});
+
+=keyword spice
+
+You can predefine L<DDG::ZeroClickInfo::Spice> parameters for usage in
+L</test_spice>.
+
+This function can be used several times to change specific defaults on the
+fly.
+
+=cut
+
+	$stash->add_symbol('&spice', sub {
+		if (ref $_[0] eq 'HASH') {
+			for (keys %{$_[0]}) {
+				$spice_params{$_} = $_[0]->{$_};
+			}
+		} else {
+			while (@_) {
+				my $key = shift;
+				my $value = shift;
+				$spice_params{$key} = $value;
+			}
+		}
+	});
+
+=keyword ddg_spice_test
+
+With this function you can easily generate a small own L<DDG::Block> for
+testing your L<DDG::Spice> alone or in combination with others.
+
+  ddg_spice_test(
+    [qw( DDG::Spice::MySpice )],
+    'myspice data' => test_spice('/js/spice/my_spice/data'),
+    'myspice data2' => test_spice('/js/spice/my_spice/data2'),
+  );
+
+=cut
+
+	$stash->add_symbol('&ddg_spice_test', sub { block_test(sub {
+		my $query = shift;
+		my $answer = shift;
+		my $spice = shift;
+		if ($answer) {
+			is_deeply($answer,$spice,'Testing query '.$query);
+		} else {
+			fail('Expected result but dont get one on '.$query);
+		}
+	},@_)});
+
+=keyword alt_to_test
+
+Use this function to verify your spice's alt_to definitions:
+
+	alt_to_test('DDG::Spice::My::Spice', [qw(alt1 alt2 alt3)]);
+
+This would check for the following:
+
+	callbacks 'ddg_spice_my_alt[123]'
+	paths '/js/spice/my/alt[123]/'
+
+=cut
+
+	$stash->add_symbol('&alt_to_test', sub {
+		my ($spice, $alt_tos) = @_;
+
+		load_class($spice);
+
+		my $rewrites = $spice->alt_rewrites;
+		ok($rewrites, "$spice has rewrites");
+
+		ok($spice =~ /^(DDG.+::)/, "Extract base from $spice");
+		my $base = $1;
+
+		for my $alt (@$alt_tos){
+			my ($cb, $path) = @{DDG::Meta::ZeroClickInfoSpice::params_from_target("$base$alt")};
+			my $rw = $rewrites->{$alt};
+			ok($rw, "$alt exists");
+			ok($rw->callback eq $cb, "$alt callback");
+			ok($rw->path eq $path, "$alt path");
+		}
+	});
+}
+
+1;
